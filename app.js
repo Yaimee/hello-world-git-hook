81f01f8aa3468cabcd3d832f852c359e33b0a299
R.Ø.M <rasmusorum@gmail.com>
2024-08-01 21:07:37 +0200

    hej

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    console.log("Hello World!");
     res.send("Hello World!");
 });
+
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
